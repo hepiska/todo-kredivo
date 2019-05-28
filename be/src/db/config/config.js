3f4760b28e238c77ea04b52a7760bedddecee8d4
hepiska <ego1403@gmail.com>
2019-05-28 16:16:56 +0700

    kwyboar bumber on gopay

--- be/src/db/config/config.js
+++ be/src/db/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "logging": false,
   },
   "production": {
-    "use_env_variable": process.env.DATABASE_URL,
+    "use_env_variable": "postgres://rlmsljsoztsomh:38c7aa27c655a3a732250bdab6aa07ee47d231cabc209b7af2f6c6c5e5b128b2@ec2-23-23-228-132.compute-1.amazonaws.com:5432/debtehog2jkgp6",
     // "password": process.env.DB_PASSWORD || "",
     // "database": process.env.DATABASE || "",
     // "host": process.env.HOST || "",
