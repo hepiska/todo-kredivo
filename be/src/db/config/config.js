b6bb74730338d83884b3fcbd5c307d97fffcbe52
hepiska <ego1403@gmail.com>
2019-05-28 19:41:45 +0700

    add readme

--- be/src/db/config/config.js
+++ be/src/db/config/config.js
@@ -2,12 +2,13 @@ require("dotenv").config()
 
 module.exports = {
   "development": {
-    "username": "ego",
-    "password": "ego",
-    "database": "todo_dev",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME || "",
+    "password": process.env.DB_PASSWORD || "",
+    "database": process.env.DATABASE || "",
+    "host": process.env.HOST || "",
+    "port": process.env.PORT || "5432",
     "dialect": "postgres",
-    "logging": true,
+    "logging": false,
   },
   "test": {
     "username": "ego",
@@ -26,13 +27,4 @@ module.exports = {
     "dialect": "postgres",
     "logging": false
   },
-  // "production": {
-  //   "username": process.env.DB_USERNAME || "rlmsljsoztsomh",
-  //   "password": process.env.DB_PASSWORD || "38c7aa27c655a3a732250bdab6aa07ee47d231cabc209b7af2f6c6c5e5b128b2",
-  //   "database": process.env.DATABASE || "debtehog2jkgp6",
-  //   "host": process.env.HOST || "ec2-23-23-228-132.compute-1.amazonaws.com",
-  //   "port": process.env.PORT || "5432",
-  //   "dialect": "postgres",
-  //   "logging": false
-  // }
 }
---
+++ readme.md
@@ -0,0 +1,25 @@
+# simple todo list
+
+## be 
+### run stage 
+1. npm i 
+2. create database in your local devices
+3. create .env file you can se .env.example
+4. run "npm run migrate-dev"
+5. run "npm run watch"
+
+### run test 
+1. create database in your local devices
+3. change db confjig file in './src/db/config.config.js' according to your db seting
+4. run 'npm run migrate-test'
+5. run "npm run seed-test
+6. run "npm test"
+
+
+* all above assuming already install sequilize cli globaly
+
+
+## fe
+1. npm i
+2. npm start
+3. the app will run on port 8083
