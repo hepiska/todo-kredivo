112fab6289a75ad7f457687a718937a204bfcc65
hepiska <ego1403@gmail.com>
2019-05-29 12:18:39 +0700

    still try to run heroku

--- be/dist/db/config/config.js
+++ be/dist/db/config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "use_env_variable": process.env.DATABASE_URL,
+    "use_env_variable": process.env.DATABASE_URL || "postgres://rlmsljsoztsomh:38c7aa27c655a3a732250bdab6aa07ee47d231cabc209b7af2f6c6c5e5b128b2@ec2-23-23-228-132.compute-1.amazonaws.com:5432/debtehog2jkgp6",
     // "username": process.env.DB_USERNAME || "rlmsljsoztsomh",
     // "password": process.env.DB_PASSWORD || "38c7aa27c655a3a732250bdab6aa07ee47d231cabc209b7af2f6c6c5e5b128b2",
     // "database": process.env.DATABASE || "debtehog2jkgp6",
